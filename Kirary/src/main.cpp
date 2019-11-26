da542e5884090aac7f092d52496ecdeab16d9783
Henrik sandberg <henrik.a.sandberg@icloud.com>
2019-11-26 19:48:39 +0100

    Generated code for pump

--- Kirary/src/main.cpp
+++ Kirary/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <analogWrite.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <SPI.h>
@@ -9,13 +10,17 @@
 #define FIREBASE_AUTH "OC47waZR7yjVfACbypdakuotyxwinkNLfqkGnV0I"
 
 //Wi-Fi connection
-const char* ssid =  "HenriksNyeNettverk"; //"Student";
-const char* pass = "G4rNAU6HwwuXXaDXwbEKovLGzhbq6Tq"; //"Kristiania1914";
+const char* ssid = "Student"; //"HenriksNyeNettverk"; //
+const char* pass = "Kristiania1914"; //"G4rNAU6HwwuXXaDXwbEKovLGzhbq6Tq"; //
 
 WiFiClient espClient;
 FirebaseData firebaseData;
 
-const int temprature_pin = 35;
+//PINS
+const int motor_pin = 18;
+const int temprature_pin = 34;
+const int moister_pin = 39;
+
 double celcius = 0.00;
 int moister_levle = 0;
 
@@ -28,11 +33,16 @@ void connect_to_wifi();
 void push_temprature(double indata);
 void update_temprature();
 void read_moister();
+void water_plant();
+void should_plant_get_water();
 
 void setup()
 {
   Serial.begin(9600);
-  
+}
+
+
+/*
   connect_to_wifi();
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
@@ -42,14 +52,20 @@ void setup()
   
   //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
   Firebase.setwriteSizeLimit(firebaseData, "small");
-}
+*/
+
 
 void loop()
 {
+  read_moister();
+  should_plant_get_water();
+}
+
+/*
   update_temprature();
   read_moister();
   delay(1000);
-}
+*/
 
 void connect_to_wifi()
 {
@@ -64,9 +80,29 @@ void connect_to_wifi()
   Serial.println("Wi-fi is now connected");
 }
 
+void should_plant_get_water()
+{
+  if (moister_levle >= 3000)
+  {
+    water_plant();
+    Firebase.setBool(firebaseData, path + DEVICE_ID +"/watering", true);
+  }
+  else
+  {
+    Firebase.setBool(firebaseData, path + DEVICE_ID +"/watering", false);
+  } 
+}
+
+void water_plant()
+{
+  analogWrite(motor_pin, 255);
+  delay(5000);
+  analogWrite(motor_pin, 0);
+}
+
 void read_moister()
 {
-  int val = analogRead(39);
+  int val = analogRead(moister_pin);
   Serial.print(val);
   Serial.println(" moister");
   if (val > moister_levle + 10) 
@@ -109,27 +145,3 @@ void push_temprature(double indata)
   Serial.println("°C");
   Firebase.setDouble(firebaseData, path + DEVICE_ID +"/temprature", celcius); //TODO: Maybe make history data here
 }
-
-//MARK: - Code for the future
-//int pinOut = 10;
-
-//Kode for turning on and off water pump
-/* TODO: Not working, may need to finde the correct pin....
-int pinOut = 35;
-
-void setup() {
-  Serial.begin(9600);
-}
-
-void loop() {
-
-  //Kode for turning on and off water pump
-  digitalWrite(pinOut, HIGH);
-  Serial.println("HIGH");
-  delay(5000);
-  digitalWrite(pinOut, LOW);
-  Serial.println("LOW");
-  delay(5000);
-
-}
-*/
